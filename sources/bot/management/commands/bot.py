b2717c45b4662194eab79510cb32b2057007a4ff
Mikhail Katulin <miksarus@jinr.ru>
2021-02-19 12:06:44 +0300

    created simple bot with tutorial

---
+++ sources/bot/management/commands/bot.py
@@ -0,0 +1,93 @@
+from os import error
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from telegram import Bot
+from telegram import Update
+from telegram.bot import log
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
+from telegram.utils.request import Request
+
+from bot.models import Message
+from bot.models import Profile
+
+
+def log_errors(f):
+    def inner(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            error_message = f"Произошла ошибка: {e}"
+            print(error_message)
+            raise e
+
+    return inner
+
+
+@log_errors
+def do_echo(updater: Update, context: CallbackContext):
+    chat_id = updater.message.chat_id
+    text = updater.message.text
+
+    p, is_new = Profile.objects.get_or_create(
+        external_id=chat_id,
+        defaults={
+            'name': updater.message.from_user.username,
+        })
+    m = Message(profile=p, text=text)
+    m.save()
+
+    reply_text = "Вера, я знаю, что это ты. Я записал, всё что ты мне пишешь в сверхнадёжную базу! Уха-ха-ха!!!"
+    updater.message.reply_text(text=reply_text)
+
+
+@log_errors
+def do_count(updater: Update, context: CallbackContext):
+    #print(updater)
+    chat_id = updater.message.chat_id
+    p, is_new = Profile.objects.get_or_create(
+        external_id=chat_id,
+        defaults={
+            'name': updater.message.from_user.username,
+        })
+    count = Message.objects.filter(profile=p).count()
+
+    updater.message.reply_text(
+        text=f"От вас было отправлено {count} сообщений", )
+
+
+class Command(BaseCommand):
+    help = 'Телеграм-бот'
+
+    def handle(self, *args, **options):
+        # 1 -- правильное подключение
+        request = Request(
+            connect_timeout=0.5,
+            read_timeout=1.0,
+        )
+        bot = Bot(
+            request=request,
+            token=settings.TOKEN,
+            base_url=settings.PROXY_URL,
+        )
+        #print(bot.get_me())
+
+        # 2 -- обработка сообщений
+        updater = Updater(
+            bot=bot,
+            use_context=True,
+        )
+
+        command_handler = CommandHandler('count', do_count)
+        updater.dispatcher.add_handler(command_handler)
+
+        message_handler = MessageHandler(Filters.text, do_echo)
+        updater.dispatcher.add_handler(message_handler)
+
+        # 3 -- запустить бесконечную обработку входящих сообщений
+        updater.start_polling()
+        updater.idle()
---
+++ sources/bot/models.py
@@ -0,0 +1,37 @@
+from enum import unique
+from django.db import models
+
+
+class Profile(models.Model):
+    external_id = models.PositiveIntegerField(
+        verbose_name="ID пользователя в соц сети",
+        unique=True,
+    )
+    name = models.TextField(verbose_name="Имя пользователя")
+
+    def __str__(self):
+        return f"{self.name} (@{self.external_id})"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+
+class Message(models.Model):
+    profile = models.ForeignKey(
+        to='bot.Profile',
+        verbose_name='Профиль',
+        on_delete=models.PROTECT,
+    )
+    text = models.TextField(verbose_name='Текст', )
+    created_at = models.DateTimeField(
+        verbose_name='Время получения',
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f"Сообщение от {self.profile}: {self.text}"
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
